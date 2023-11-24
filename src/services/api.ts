a57109b11535b590c8e305e2efa9dc14d3cc0516
baniomash <jonatasdez@yahoo.com.br>
2023-11-24 16:16:05 -0300

    feat: responsivity added

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export async function getVoucher(clientCpf: string): Promise<string>{
         
-        const response = await axios.post("http://"+import.meta.env.VITE_API_NAME+":3000/voucher", {
-            clientCpf: clientCpf
-        }).catch(error => {
-            return error.message;
-        });
-        return response.data;
-        }
+    const response = await axios.post("http://"+import.meta.env.VITE_API_NAME+":3000/voucher", {
+        clientCpf: clientCpf
+    }).catch(error => {
+        return error.message;
+    });
+    return response.data;
+}
