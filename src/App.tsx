c0c54082a35e913b7a09785743f2e8c74412188e
baniomash <jonatase123.je@gmail.com>
2023-12-29 13:46:30 -0300

    feat: changing text color

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ function App() {
     <div>
       <main className='container'>
         <img className="image" src={imageLoja} alt="imagem da loja" />
-        {data?.done ? <ResultSection voucher={data.voucher}/>
+        {true ? <ResultSection voucher="165165148486515"/>
+        // {data?.done ? <ResultSection voucher={data.voucher}/>
           : <FormSection changeData={changeData} cpfInput={cpfInput} setCpfInput={setCpfInput} setData={setData} />}
       </main>
     </div>
